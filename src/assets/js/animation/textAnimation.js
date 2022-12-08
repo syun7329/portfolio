36e848c5a700ef704d36b0f8fb05ffc475f9a847
YonedaKan <obscure723@gmail.com>
2022-12-08 23:55:43 +0900

    Update textAnimation.js

--- src/assets/js/animation/textAnimation.js
+++ src/assets/js/animation/textAnimation.js
@@ -2,13 +2,17 @@ const jsText = ".js-textAnimation span";
 const targets = gsap.utils.toArray(".js-textAnimation");
 
 targets.forEach((target) => {
+
   target.innerHTML = splitText(target);
 
-  gsap.set(jsText, {
+  const q = gsap.utils.selector(target)
+  const span = q('span')
+
+  gsap.set(span, {
     y: 200,
   });
 
-  gsap.to(jsText, {
+  gsap.to(span, {
     y: 0,
     stagger: 0.05,
     duration: 0.5,
@@ -17,8 +21,8 @@ targets.forEach((target) => {
     scrollTrigger: {
       trigger: target,
       start: "top center",
-      once: false,
-      // markers: true,
+      once: true,
+      markers: true,
     },
   });
 });
